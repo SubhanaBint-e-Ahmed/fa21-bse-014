b5a3e0c3fb9646ac5d31bf217496abd2c6bda81d
SubhanaBint-e-Ahmed <160214637+SubhanaBint-e-Ahmed@users.noreply.github.com>
2024-02-25 21:16:24 +0500

    stringMethod

--- Assignments/stringMethod.js
+++ Assignments/stringMethod.js
@@ -1,1 +1,18 @@
+/**Uppercase Method */
+let name="subhana";
+console.log(name.toUpperCase());
 
+/**lowercase Method */
+console.log(name.toLowerCase());
+
+/** slice method */
+console.log(name.slice(1,4))/**with 2 parameters */
+console.log(name.slice(2))/** with 1 parameter */
+/**replace method */
+console.log(name.replace("u","o"))
+/**concat method */
+let friend="zara"
+console.log(name.concat(" is a friend of ", friend,"khan"))
+/**trim method */
+let game="   cricket  "
+console.log(game.trim())
